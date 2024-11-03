364fd60727bc1fc3d0c999b15824c45cd4a43a35
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-11-03 19:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
+ let row=document.createElement("tr");
+ 	let cell1=document.createElement("td");
+	cell1.innerHTML='New Cell1';
+	row.append(cell1);
+	let cell2=document.createElement("td");
+	cell2.innerHTML='New Cell2';
+	row.append(cell2);
+
+	
+   document.getElementById("sampleTable").prepend(row);
   
 }
+
+// document.querySelector("input").addEventListner('click',insert_Row);
